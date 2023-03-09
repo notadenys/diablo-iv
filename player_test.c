d03d9dd12d61f13392bea1acfa35bc4313a1b182
IvanOsadtsiv <ivan.osadtsiv1@gmail.com>
2023-03-09 16:50:30 +0100

    Create player_test.c

---
+++ player_test.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "player.h"
+
+
+int main (int argc, char * argv[]) 
+{
+    int x_test = 5;
+    int y_test = 5;
+
+    player_t* plyr_test = createPlayer (x_test, y_test);
+    printf (toStringPl (plyr_test));
+    replacement (plyr_test);
+    printf (toStringPl (plyr_test));
+
+return EXIT_SUCCESS;
+}
+// gcc -c player.c -o player.o
+// gcc player_test.c player.o -o main
+// ./main
