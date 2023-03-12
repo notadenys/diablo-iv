5c4fd610c7e4ae9293910293b2331c1347a87902
IvanOsadtsiv <ivan.osadtsiv1@gmail.com>
2023-03-12 14:19:53 +0100

    Update player_test.c
    Ended version of player_test.c

--- player_test.c
+++ player_test.c
@@ -11,10 +11,11 @@ int main (int argc, char * argv[])
 
     player_t* plyr_test = createPlayer (x_test, y_test);
     printf (toStringPl (plyr_test));
+    // output_testing(plyr_test);
     replacement (plyr_test);
     printf (toStringPl (plyr_test));
 
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 // gcc -c player.c -o player.o
 // gcc player_test.c player.o -o main
