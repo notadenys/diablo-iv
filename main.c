6c3ef3f1d3178948e19f74daaa9071ae6031722f
Denys <sh.denys.ev@gmail.com>
2023-03-20 21:35:21 +0100

    added main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/// @file       main.c
+/// @author     Ivan OSADTSIV, Denys SHCHERBA.
+/// @brief      main file of the game
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "const.h"
+#include "monster.h"
+#include "player.h"
+#include "output.h"
+
+
+int main()
+{
+
+    return EXIT_SUCCESS;
+}
